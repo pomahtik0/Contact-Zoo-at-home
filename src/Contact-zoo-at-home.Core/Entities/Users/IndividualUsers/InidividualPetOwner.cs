ac5cf0d1907a1759d953e46332453aada6aa9625
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-22 12:38:07 +0200

    implimenting interfaces for IndividualPetOwner

--- src/Contact-zoo-at-home.Core/Entities/Users/IndividualUsers/InidividualPetOwner.cs
+++ src/Contact-zoo-at-home.Core/Entities/Users/IndividualUsers/InidividualPetOwner.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contact_zoo_at_home.Core.Entities.Pets;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Contact_zoo_at_home.Core.Entities.Users.IndividualUsers
 {
     internal class InidividualPetOwner : IUser, IPetOwner
     {
+        public string FullName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string UserName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Password { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public byte[] ProfileImage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? ContactPhone { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? ContactEmail { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IEnumerable<Pet> OwnedPets { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
