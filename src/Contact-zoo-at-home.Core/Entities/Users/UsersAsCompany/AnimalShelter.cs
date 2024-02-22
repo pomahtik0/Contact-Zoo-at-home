6f753cbccb382590bd25b649372d91f68ee50cd9
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-22 15:18:57 +0200

    implimenting company interfaces (no realization)

--- src/Contact-zoo-at-home.Core/Entities/Users/UsersAsCompany/AnimalShelter.cs
+++ src/Contact-zoo-at-home.Core/Entities/Users/UsersAsCompany/AnimalShelter.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Contact_zoo_at_home.Core.Entities.Contracts;
+using Contact_zoo_at_home.Core.Entities.Pets;
+using Contact_zoo_at_home.Core.Entities.Users.IndividualUsers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,15 @@ namespace Contact_zoo_at_home.Core.Entities.Users.UsersAsCompany
 {
     internal class AnimalShelter : ICompany, IPetOwner
     {
+        public IEnumerable<CompanyPetRepresentative> CompanyPetRepresentatives { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IEnumerable<IContract> ActiveContracts { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IEnumerable<IContract> ArchivedContracts { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string FullName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string UserName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Password { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public byte[] ProfileImage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? ContactPhone { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? ContactEmail { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IEnumerable<Pet> OwnedPets { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
