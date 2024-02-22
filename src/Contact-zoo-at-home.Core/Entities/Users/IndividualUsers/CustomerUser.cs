0eb2034da0a215403fc25e0bf137ce2df6c9198e
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-22 11:27:51 +0200

    users have active contracts

--- src/Contact-zoo-at-home.Core/Entities/Users/IndividualUsers/CustomerUser.cs
+++ src/Contact-zoo-at-home.Core/Entities/Users/IndividualUsers/CustomerUser.cs
@@ -8,5 +8,12 @@ namespace Contact_zoo_at_home.Core.Entities.Users.IndividualUsers
 {
     internal class CustomerUser : IUser
     {
+        public System.Collections.Generic.IEnumerable<Contact_zoo_at_home.Core.Entities.Contracts.IContract> ActiveContracts
+        {
+            get => default;
+            set
+            {
+            }
+        }
     }
 }
