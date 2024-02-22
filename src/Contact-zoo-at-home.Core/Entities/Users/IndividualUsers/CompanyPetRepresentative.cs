32f7c056672aaea30e2c8be980ec4f58bfde9b6c
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-22 12:13:46 +0200

    solving pet representative interface for CompanyPetRepresentative

--- src/Contact-zoo-at-home.Core/Entities/Users/IndividualUsers/CompanyPetRepresentative.cs
+++ src/Contact-zoo-at-home.Core/Entities/Users/IndividualUsers/CompanyPetRepresentative.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contact_zoo_at_home.Core.Entities.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Contact_zoo_at_home.Core.Entities.Users.IndividualUsers
 {
-    internal class CompanyPetRepresentative : IUser
+    internal class CompanyPetRepresentative : IUser, IPetRepresentative
     {
         private string _fullName = "";
 
@@ -20,12 +21,15 @@ namespace Contact_zoo_at_home.Core.Entities.Users.IndividualUsers
 
         private string? _contactEmail = "";
 
+        private IEnumerable<IContract> _activeContracts = [];
+
         public string FullName { get => _fullName; set => _fullName = value; }
         public string UserName { get => _userName; set => _userName = value; }
         public string Password { get => _password; set => _password = value; }
         public byte[] ProfileImage { get => _profileImage; set => _profileImage = value; }
         public string? ContactPhone { get => _contactPhone; set => _contactPhone = value; }
         public string? ContactEmail { get => _contactEmail; set => _contactEmail = value; }
+        public virtual IEnumerable<IContract> ContractsToRepresent { get => _activeContracts; set => _activeContracts = value; }
         public ICompany Company { get; set; }
     }
 }
