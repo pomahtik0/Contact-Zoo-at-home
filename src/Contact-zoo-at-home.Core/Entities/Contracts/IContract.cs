4d12655da337f0566cfbb33e0ce6d58fd42a8016
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-22 17:46:01 +0200

    someBase contract logic

--- src/Contact-zoo-at-home.Core/Entities/Contracts/IContract.cs
+++ src/Contact-zoo-at-home.Core/Entities/Contracts/IContract.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Contact_zoo_at_home.Core.Entities.Pets;
+using Contact_zoo_at_home.Core.Entities.Users;
+using Contact_zoo_at_home.Core.Entities.Users.IndividualUsers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,16 @@ using System.Threading.Tasks;
 
 namespace Contact_zoo_at_home.Core.Entities.Contracts
 {
+    //TODO: seperate interface for contractors
     internal interface IContract
     {
+        CustomerUser Customer { get; set; }
+        IPetOwner Contractor { get; set; }
+        IEnumerable<Pet> PetsInContract {  get; set; }
+
+        // Other data: date, paymentstatus, ect.
+
+        void CustomerAccepts(CustomerUser customer); // throw if something went wrong
+        void ContractorAccepts(IPetOwner contractor); // notify customer after
     }
 }
