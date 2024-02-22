1667f2a64581185d104de62bf585de760a0c287f
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-22 11:43:47 +0200

    basic implimentation of the interface for company pet representatives

--- src/Contact-zoo-at-home.Core/Entities/Users/IndividualUsers/CompanyPetRepresentative.cs
+++ src/Contact-zoo-at-home.Core/Entities/Users/IndividualUsers/CompanyPetRepresentative.cs
@@ -8,5 +8,11 @@ namespace Contact_zoo_at_home.Core.Entities.Users.IndividualUsers
 {
     internal class CompanyPetRepresentative : IUser
     {
+        public string FullName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string UserName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Password { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public byte[] ProfileImage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? ContactPhone { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? ContactEmail { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
