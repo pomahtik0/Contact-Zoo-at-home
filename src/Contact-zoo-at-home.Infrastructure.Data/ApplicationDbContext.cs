a45d5e356d9084c23e9df17b846c6feb91c72778
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-24 12:40:21 +0200

    basic dbcontext

---
+++ src/Contact-zoo-at-home.Infrastructure.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contact_zoo_at_home.Core.Entities.Users;
+using Contact_zoo_at_home.Core.Entities.Pets;
+using Contact_zoo_at_home.Core.Entities.Contracts;
+
+namespace Contact_zoo_at_home.Infrastructure.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<IUser> Users { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<IContract> Contracts { get; set; }
+    }
+}
