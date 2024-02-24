9f614e9484e0d08cdaaef9ae15dd835d9f80574f
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-24 12:12:29 +0200

    pet's base properties

--- src/Contact-zoo-at-home.Core/Entities/Pets/Pet.cs
+++ src/Contact-zoo-at-home.Core/Entities/Pets/Pet.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contact_zoo_at_home.Core.Entities.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace Contact_zoo_at_home.Core.Entities.Pets
 {
     internal abstract class Pet
     {
+        public IPetOwner? Owner { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ShortDescription { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public float Rating { get; set; }
+        public int RaitingUserVotesCount {  get; set; }
+        public double Price { get; set; }
+        public string Species { get; set; } = string.Empty; // Replace with enum mb
     }
 }
