6e6e7a45576674fec73ed832c7f6ec16aa6af17a
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-24 09:25:04 +0200

    interface for customer contractSide

---
+++ src/Contact-zoo-at-home.Core/Entities/Users/ICustomer.cs
@@ -0,0 +1,21 @@
+﻿using Contact_zoo_at_home.Core.Entities.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_zoo_at_home.Core.Entities.Users
+{
+    /// <summary>
+    /// Interface for those who can create contracts.
+    /// </summary>
+    internal interface ICustomer
+    {
+        IEnumerable<IContract> ActiveContracts { get; }
+
+        void CreateContract(IContract contract);
+        void AcceptContract(IContract contract);
+        void DeclineContract(IContract contract);
+    }
+}
