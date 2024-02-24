70cc7702177f7df49222b89e5e69adbb30f505c4
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-24 11:28:08 +0200

    notifications deleted (probably a part of smt else)

--- src/Contact-zoo-at-home.Core/Entities/Contracts/IContract.cs
+++ src/Contact-zoo-at-home.Core/Entities/Contracts/IContract.cs
@@ -23,8 +23,5 @@ namespace Contact_zoo_at_home.Core.Entities.Contracts
         void ContractClosed();
         void ContractDeclinedByContractor(IContractor contractor);
         void ContractDeclinedByCustomer(ICustomer customer);
-        void NotifyAllParties(string messageTitle, string messageBody);
-        void NotifyCustomer(string messageTitle, string messageBody);
-        void NotifyContractor(string messageTitle, string messageBody);
     }
 }
