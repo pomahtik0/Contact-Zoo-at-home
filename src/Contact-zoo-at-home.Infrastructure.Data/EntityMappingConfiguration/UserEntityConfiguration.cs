8e3bb69fe0d242451c22d2e0a1c217f658e9e978
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-24 13:04:06 +0200

    UserEntityConfiguration init

---
+++ src/Contact-zoo-at-home.Infrastructure.Data/EntityMappingConfiguration/UserEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Contact_zoo_at_home.Core.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_zoo_at_home.Infrastructure.Data.EntityMappingConfiguration
+{
+    internal class UserEntityConfiguration : IEntityTypeConfiguration<IUser>
+    {
+        public void Configure(EntityTypeBuilder<IUser> builder)
+        {
+        }
+    }
+}
