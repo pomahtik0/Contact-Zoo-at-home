98502d6f9becfebde14f2a387de17447d8c7fc7c
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-24 16:02:05 +0200

    Pets configuration

---
+++ src/Contact-zoo-at-home.Infrastructure.Data/EntityMappingConfiguration/PetEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Contact_zoo_at_home.Core.Entities.Pets;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_zoo_at_home.Infrastructure.Data.EntityMappingConfiguration
+{
+    internal class PetEntityConfiguration : IEntityTypeConfiguration<Pet>
+    {
+        public void Configure(EntityTypeBuilder<Pet> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.ToTable("Pets")
+                .HasDiscriminator(e => e.Species)
+                .HasValue<Dog>("Dog")
+                .HasValue<Cat>("Cat");
+        }
+    }
+}
