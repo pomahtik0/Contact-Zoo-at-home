6eb1ac9810a85506141dbcc545c052571b1d0d44
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-24 14:46:28 +0200

    new DbSets

--- src/Contact-zoo-at-home.Infrastructure.Data/ApplicationDbContext.cs
+++ src/Contact-zoo-at-home.Infrastructure.Data/ApplicationDbContext.cs
@@ -4,26 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Contact_zoo_at_home.Core.Entities.Users;
+using Contact_zoo_at_home.Core.Entities.Users.IndividualUsers;
 using Contact_zoo_at_home.Core.Entities.Pets;
 using Contact_zoo_at_home.Core.Entities.Contracts;
 using Contact_zoo_at_home.Infrastructure.Data.EntityMappingConfiguration;
+using Contact_zoo_at_home.Core.Entities.Users.UsersAsCompany;
 
 namespace Contact_zoo_at_home.Infrastructure.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<IUser> Users { get; set; }
+        public DbSet<CustomerUser> CustomerUsers { get; set; }
+        public DbSet<InidividualPetOwner> InidividualPetOwners { get; set; }
+        public DbSet<CompanyPetRepresentative> PetRepresentatives { get; set; }
+        public DbSet<ZooShop> ZooShops { get; set; }
+        public DbSet<AnimalShelter> AnimalShelters { get; set; }
+
         public DbSet<Pet> Pets { get; set; }
-        public DbSet<IContract> Contracts { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(); // insert connection string
+            optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\pomahtik\\source\\repos\\Contact-zoo-at-home\\src\\Contact-zoo-at-home.Infrastructure.Data\\Database1.mdf;Integrated Security=True;Connect Timeout=30"); // insert connection string
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            UserEntityConfiguration.Configure(modelBuilder);
+
         }
     }
 }
