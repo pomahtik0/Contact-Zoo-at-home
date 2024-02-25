c175ded060c4c60f95236843ed8b9f7254e3ad12
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-25 14:43:08 +0200

    Revert "removed outdated files"
    This reverts commit 27067b32c187eebe7778ceb08c3ea86c119b5b0c.

---
+++ src/Contact-zoo-at-home.Infrastructure.Data/EntityMappingConfiguration/CustomerUserEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Contact_zoo_at_home.Core.Entities.Contracts;
+using Contact_zoo_at_home.Core.Entities.Users;
+using Contact_zoo_at_home.Core.Entities.Users.IndividualUsers;
+using Contact_zoo_at_home.Core.Entities.Users.UsersAsCompany;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_zoo_at_home.Infrastructure.Data.EntityMappingConfiguration
+{
+    internal class CustomerUserEntityConfiguration : IEntityTypeConfiguration<CustomerUser>
+    {
+        public void Configure(EntityTypeBuilder<CustomerUser> builder)
+        {
+            builder.ToTable("Users").HasDiscriminator<string>("UserType").HasValue("Customer").IsComplete(false);
+            builder.HasKey(e => e.Id);
+            builder.Ignore(e => e.ActiveContracts); // ignore contracts for now
+        }
+    }
+}
