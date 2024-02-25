ff96510c020c86948a9245d5098a0f37790bd61a
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-25 14:09:21 +0200

    abstract user fix

--- src/Contact-zoo-at-home.Core/Entities/Users/AbstractUser.cs
+++ src/Contact-zoo-at-home.Core/Entities/Users/AbstractUser.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Contact_zoo_at_home.Core.Entities.Users
 {
-    public class AbstractUser
+    public abstract class AbstractUser
     {
         public int Id { get; set; }
         public string FullName { get; set; }
