0f4cd209adb2f164301efb77e6ea080b9236c100
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-25 14:38:37 +0200

    company entity configuration file

---
+++ src/Contact-zoo-at-home.Infrastructure.Data/EntityMappingConfiguration/CompanyEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Contact_zoo_at_home.Core.Entities.Pets;
+using Contact_zoo_at_home.Core.Entities.Users;
+using Contact_zoo_at_home.Core.Entities.Users.IndividualUsers;
+using Contact_zoo_at_home.Core.Entities.Users.UsersAsCompany;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_zoo_at_home.Infrastructure.Data.EntityMappingConfiguration
+{
+    internal class CompanyEntityConfiguration : IEntityTypeConfiguration<AbstractCompany>
+    {
+        public void Configure(EntityTypeBuilder<AbstractCompany> builder)
+        {
+
+        }
+    }
+}
