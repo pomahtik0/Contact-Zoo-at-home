692d533dbf4812926b80a4223beca85f8219cc0e
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-25 14:38:57 +0200

    User entity confiruration file

---
+++ src/Contact-zoo-at-home.Infrastructure.Data/EntityMappingConfiguration/UserEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Contact_zoo_at_home.Core.Entities.Pets;
+using Contact_zoo_at_home.Core.Entities.Users;
+using Contact_zoo_at_home.Core.Entities.Users.IndividualUsers;
+using Contact_zoo_at_home.Core.Entities.Users.UsersAsCompany;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_zoo_at_home.Infrastructure.Data.EntityMappingConfiguration
+{
+    internal class UserEntityConfiguration : IEntityTypeConfiguration<AbstractUser>
+    {
+        public void Configure(EntityTypeBuilder<AbstractUser> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.ToTable("Users")
+                .HasDiscriminator<string>("userType")
+                .HasValue<CustomerUser>("Customer")
+                .HasValue<IndividualPetOwner>("IndividualPetOwner")
+                .HasValue<CompanyPetRepresentative>("CompanyPetRepresentative")
+                .HasValue<AnimalShelter>("AnimalShelter")
+                .HasValue<ZooShop>("ZooShop");
+        }
+    }
+}
