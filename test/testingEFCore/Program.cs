66eba89452e8f2f9f6e86540d6b127ed848b2035
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-29 22:55:07 +0200

    created project to make some simple DB tests

---
+++ test/testingEFCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace testingEFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
