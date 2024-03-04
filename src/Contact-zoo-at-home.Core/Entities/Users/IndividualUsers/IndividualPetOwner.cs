513da02199f5d9b97859d94341a49506b92b393f
Roman Havriluk <pomahtik_inc@outlook.com>
2024-03-04 10:28:58 +0200

    individual pet owner logic

--- src/Contact-zoo-at-home.Core/Entities/Users/IndividualUsers/IndividualPetOwner.cs
+++ src/Contact-zoo-at-home.Core/Entities/Users/IndividualUsers/IndividualPetOwner.cs
@@ -2,6 +2,7 @@
 using Contact_zoo_at_home.Core.Entities.Pets;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,53 @@ namespace Contact_zoo_at_home.Core.Entities.Users.IndividualUsers
 {
     public class IndividualPetOwner : BaseUser, IPetOwner, IPetRepresentative, IContractor
     {
-        public IEnumerable<BasePet> OwnedPets { get; } = [];
+
+        #region PetsLogic (IPetOwner)
+
+        // mind, you can't remove ownership.
+
+        private IList<BasePet> _ownedPets = [];
+        public IEnumerable<BasePet> OwnedPets { get => new ReadOnlyCollection<BasePet>(_ownedPets); }
+        public void AddPet(BasePet pet)
+        {
+            if (_ownedPets.Any(_pet => _pet.Equals(pet)))
+            {
+                throw new InvalidOperationException("Pet is already owned by this pet representative");
+            }
+            _ownedPets.Add(pet);
+            pet.Owner = this;
+        }
+
+        #endregion
+
+        #region ContractsToRepresent (IPetRepresentative)
 
         public IEnumerable<BaseContract> ContractsToRepresent { get; } = [];
 
-        public IEnumerable<BaseContract> ActiveContracts { get; } = [];
+        #endregion
+
+        #region ActiveContracts logic (IContractor)
+
+        private IList<BaseContract> _activeContracts = [];
+        public IEnumerable<BaseContract> ActiveContracts { get => new ReadOnlyCollection<BaseContract>(_activeContracts); }
 
         public void AcceptContract(BaseContract contract)
         {
             throw new NotImplementedException();
         }
+
+        public void AddNewContract(BaseContract contract)
+        {
+            if (_activeContracts.Any(_contract => _contract.Equals(contract)))
+            {
+                throw new ArgumentException("Contract is already added", nameof(contract));
+            }
+
+            // ToDo: Set contract as UnderConsideration
+            _activeContracts.Add(contract);
+            contract.Contractor = this;
+        }
+
 
         public void CloseContract(BaseContract contract)
         {
@@ -35,5 +73,8 @@ namespace Contact_zoo_at_home.Core.Entities.Users.IndividualUsers
         {
             throw new NotImplementedException();
         }
+
+        #endregion
+
     }
 }
