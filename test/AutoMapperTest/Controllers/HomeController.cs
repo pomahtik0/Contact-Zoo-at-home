bcb4c226e7e7fe7fea8dd0e7734f427c6eac2211
Roman Havriluk <pomahtik_inc@outlook.com>
2024-03-06 09:28:31 +0200

    created new project to make tests on AutoMapper

---
+++ test/AutoMapperTest/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using AutoMapperTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AutoMapperTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
