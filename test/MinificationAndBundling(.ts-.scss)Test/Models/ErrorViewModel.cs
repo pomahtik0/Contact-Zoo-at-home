9f3a85aa12246abc4f471aa180c6ce661a394ed0
Roman Havriluk <pomahtik_inc@outlook.com>
2024-03-06 12:08:14 +0200

    new project to test support of .ts .scss + minification + bunling

---
+++ test/MinificationAndBundling(.ts-.scss)Test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinificationAndBundling_.ts_.scss_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
