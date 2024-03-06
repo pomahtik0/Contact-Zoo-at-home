d5dab9dc15d724b3d2f57e029bd9a89919ad9867
Roman Havriluk <pomahtik_inc@outlook.com>
2024-03-06 12:35:39 +0200

    simplified controller and layout

--- test/MinificationAndBundling(.ts-.scss)Test/Controllers/HomeController.cs
+++ test/MinificationAndBundling(.ts-.scss)Test/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace MinificationAndBundling_.ts_.scss_Test.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
