6939acc74f31b8765da9a874acf32997ae000715
Roman Havriluk <pomahtik_inc@outlook.com>
2024-03-11 15:56:41 +0200

    some test with registration

--- src/WebUI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/WebUI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -11,6 +11,7 @@ using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
 using Contact_zoo_at_home.Infrastructure.Identity;
+using Contact_zoo_at_home.Infrastructure.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -27,23 +28,18 @@ namespace WebUI.Areas.Identity.Pages.Account
         private readonly SignInManager<ApplicationIdentityUser> _signInManager;
         private readonly UserManager<ApplicationIdentityUser> _userManager;
         private readonly IUserStore<ApplicationIdentityUser> _userStore;
-        private readonly IUserEmailStore<ApplicationIdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
 
         public RegisterModel(
             UserManager<ApplicationIdentityUser> userManager,
             IUserStore<ApplicationIdentityUser> userStore,
             SignInManager<ApplicationIdentityUser> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            ILogger<RegisterModel> logger)
         {
             _userManager = userManager;
             _userStore = userStore;
-            _emailStore = GetEmailStore();
             _signInManager = signInManager;
             _logger = logger;
-            _emailSender = emailSender;
         }
 
         /// <summary>
@@ -76,9 +72,11 @@ namespace WebUI.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
-            public string Email { get; set; }
+            [MinLength(4)]
+            [MaxLength(ConstantsForEFCore.Sizes.userNameLength)]
+            [Display(Name = "Username")]
+            //[Remote(action: "VerifyUserName", controller: "Users")]
+            public string Username { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -111,39 +109,19 @@ namespace WebUI.Areas.Identity.Pages.Account
         {
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            _logger.LogDebug($"{Input.Username} {Input.Password}");
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                    return LocalRedirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
                 {
@@ -167,15 +145,6 @@ namespace WebUI.Areas.Identity.Pages.Account
                     $"Ensure that '{nameof(ApplicationIdentityUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
                     $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
             }
-        }
-
-        private IUserEmailStore<ApplicationIdentityUser> GetEmailStore()
-        {
-            if (!_userManager.SupportsUserEmail)
-            {
-                throw new NotSupportedException("The default UI requires a user store with email support.");
-            }
-            return (IUserEmailStore<ApplicationIdentityUser>)_userStore;
         }
     }
 }
---
+++ src/WebUI/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Contact_zoo_at_home.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationIdentityUser> _userManager;
+        private readonly ILogger<UsersController> _logger;
+        public UsersController(ILogger<UsersController> logger, UserManager<ApplicationIdentityUser> userManager)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public async Task<IActionResult> VerifyUserName(string userName)
+        {
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                return Json("User name empty");
+            }
+            var user = await _userManager.FindByNameAsync(userName);
+            _logger.Log(LogLevel.Information, $"checking if User {userName} is already in DB");
+            if (user is not null)
+            {
+                _logger.LogInformation("true");
+                return Json($"User Name {userName} is already in use.");
+            }
+            _logger.LogInformation("false");
+            return Json(true);
+        }
+    }
+}
