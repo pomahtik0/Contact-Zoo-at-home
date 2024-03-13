6ca225683433ab2ac8168cc2561ebc398d3ac106
Roman Havriluk <pomahtik_inc@outlook.com>
2024-03-13 12:34:07 +0200

    creating usercontroller

---
+++ src/WebUI/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Contact_zoo_at_home.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly SignInManager<ApplicationIdentityUser> _signInManager;
+        private readonly UserManager<ApplicationIdentityUser> _userManager;
+        private readonly IUserStore<ApplicationIdentityUser> _userStore;
+        private readonly IMapper _mapper;
+
+        public UserController(ILogger<UserController> logger, 
+            SignInManager<ApplicationIdentityUser> signInManager, 
+            UserManager<ApplicationIdentityUser> userManager, 
+            IUserStore<ApplicationIdentityUser> userStore, 
+            IMapper mapper)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _userStore = userStore;
+            _mapper = mapper;
+        }
+    }
+}
