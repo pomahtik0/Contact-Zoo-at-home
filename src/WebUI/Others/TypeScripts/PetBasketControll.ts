47b1093b0977db1ed70f8a96baabeb1f433d513a
Roman Havriluk <pomahtik_inc@outlook.com>
2024-03-16 14:18:37 +0200

    displaying current number of pets in the 'basket'

--- src/WebUI/Others/TypeScripts/PetBasketControll.ts
+++ src/WebUI/Others/TypeScripts/PetBasketControll.ts
@@ -3,4 +3,19 @@
     if (existingCart.some(elem => elem === id)) { return; } // avoiding duplicates
     existingCart.push(id);
     sessionStorage.setItem("MyCart", JSON.stringify(existingCart));
+    showNumberOfElementsInBasket();
 }
+function showNumberOfElementsInBasket() {
+    // Get the element by ID
+    const changeMeElement = document.getElementById("petBasketBadge");
+
+    const existingCart: string[] = JSON.parse(sessionStorage.getItem("MyCart")) || [];
+    if (existingCart.length == 0) {
+        changeMeElement.textContent = "";
+    }
+    // Set the new content
+    else {
+        changeMeElement.textContent = existingCart.length.toString();
+    }
+}
+document.addEventListener("DOMContentLoaded", showNumberOfElementsInBasket)
--- src/WebUI/wwwroot/js/PetBasketControll.js
+++ src/WebUI/wwwroot/js/PetBasketControll.js
@@ -5,5 +5,19 @@ function addToCart(id) {
     } // avoiding duplicates
     existingCart.push(id);
     sessionStorage.setItem("MyCart", JSON.stringify(existingCart));
+    showNumberOfElementsInBasket();
 }
+function showNumberOfElementsInBasket() {
+    // Get the element by ID
+    var changeMeElement = document.getElementById("petBasketBadge");
+    var existingCart = JSON.parse(sessionStorage.getItem("MyCart")) || [];
+    if (existingCart.length == 0) {
+        changeMeElement.textContent = "";
+    }
+    // Set the new content
+    else {
+        changeMeElement.textContent = existingCart.length.toString();
+    }
+}
+document.addEventListener("DOMContentLoaded", showNumberOfElementsInBasket);
 //# sourceMappingURL=PetBasketControll.js.map
