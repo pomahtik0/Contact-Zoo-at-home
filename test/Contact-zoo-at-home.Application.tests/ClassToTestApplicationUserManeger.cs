4fb9a9ad56aef75adf9cbbe94bdafcf60ec76db1
Roman Havriluk <pomahtik_inc@outlook.com>
2024-03-22 11:28:18 +0200

    some playing with test initialization

--- test/Contact-zoo-at-home.Application.tests/ClassToTestApplicationUserManeger.cs
+++ test/Contact-zoo-at-home.Application.tests/ClassToTestApplicationUserManeger.cs
@@ -1,33 +1,54 @@
 using Contact_zoo_at_home.Application.Interfaces.AccountManagement;
-using Contact_zoo_at_home.Application.Realizations;
+using Contact_zoo_at_home.Application.Realizations.AccountManagement;
 using Contact_zoo_at_home.Core.Entities.Users;
 using Contact_zoo_at_home.Core.Entities.Users.IndividualUsers;
 using Contact_zoo_at_home.Infrastructure.Data;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore.Storage;
+using System.Data.Common;
 
 namespace Contact_zoo_at_home.Application.tests
 {
     [TestClass]
     public class ClassToTestApplicationUserManeger
     {
-        public const string testDbConnectionString = "Server=(localdb)\\mssqllocaldb;Database=Contact-zoo-at-home.test;Trusted_Connection=True;MultipleActiveResultSets=true";
+        private const string testDbConnectionString = "Server=(localdb)\\mssqllocaldb;Database=Contact-zoo-at-home.test;Trusted_Connection=True;MultipleActiveResultSets=true";
+        
+        private static DbConnection testConnection = null!;
+        private static ApplicationDbContext testDbContext = null!;
+        private static DbTransaction testTransaction = null!;
+        private static TestContext testContext = null!;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            testConnection = new SqlConnection(testDbConnectionString);
+            testDbContext = new ApplicationDbContext(testConnection);
+            testTransaction = testDbContext.Database.BeginTransaction().GetDbTransaction();
+            testContext = context;
+            testContext.WriteLine($"Initializing {nameof(ClassToTestApplicationUserManeger)}");
+        }
 
         [TestMethod]
-        public void UseManegerCreateNewUser_EmptyUserWithValidId_CreatesNewUser()
+        public void CreateNewUser_EmptyUserWithValidId_CreatesNewUser()
         {
             IUserManeger userManager = new UserManager();
             BaseUser customer = new CustomerUser() { Id = 1 };
 
-            using var connection = new SqlConnection(testDbConnectionString);
-            using var dbContext = new ApplicationDbContext(connection);
-            using var transaction = dbContext.Database.BeginTransaction();
+            userManager.CreateNewUserAsync(customer, testConnection, testTransaction).Wait();
 
-            userManager.CreateNewUserAsync(customer, connection, transaction.GetDbTransaction()).Wait();
-
-            var savedUser = dbContext.Users.Find(customer.Id);
+            var savedUser = testDbContext.Users.Find(customer.Id);
             Assert.IsNotNull(savedUser);
-            transaction.Rollback();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            testTransaction.Rollback();
+            testTransaction.Dispose();
+            testDbContext.Dispose();
+            testConnection.Dispose();
+            testContext.WriteLine($"Cleaning up {nameof(ClassToTestApplicationUserManeger)}");
         }
     }
 }
