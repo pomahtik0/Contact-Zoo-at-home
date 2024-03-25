354eecf92c01bff11c53a215af01b0100553e777
Roman Havriluk <pomahtik_inc@outlook.com>
2024-03-25 09:21:24 +0200

    some initialization of realization

---
+++ src/Contact-zoo-at-home.Application/Realizations/AccountManagement/CompanyManager.cs
@@ -0,0 +1,54 @@
+﻿using Contact_zoo_at_home.Core.Entities.Users;
+using Contact_zoo_at_home.Core.Entities.Users.Images;
+using Contact_zoo_at_home.Core.Entities.Users.IndividualUsers;
+using Contact_zoo_at_home.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_zoo_at_home.Application.Realizations.AccountManagement
+{
+    public class CompanyManager : PetOwnerManager
+    {
+        public CompanyManager(DbConnection? connection) : base(connection) { }
+        public CompanyManager(DbTransaction transaction) : base(transaction) { }
+
+        public async Task<Company> GetProfileInSpecifiedLanAsync(int companyId, Language language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RedactProfileInSpecifiedLanAsync(Company company, Language language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IList<Representative>> GetListOfRepresentatives(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AddRepresentativesAsync(IList<Representative> representatives, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RemoveRepresentativeAsync(int representativeId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AddNewImageAsync(CompanyImage image, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteImageAsync(int imageId, int companyId)
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
