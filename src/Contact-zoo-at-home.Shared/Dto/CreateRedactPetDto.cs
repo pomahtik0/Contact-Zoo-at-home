1a11138aff8ac2ed44c9516a04b0e7bc1adfdd5c
Roman Havriluk <pomahtik_inc@outlook.com>
2024-04-05 14:07:04 +0300

    some more dto's for pet create/redact

---
+++ src/Contact-zoo-at-home.Shared/Dto/CreateRedactPetDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contact_zoo_at_home.Shared.Dto
+{
+    public record CreateRedactPetDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string ShortDescription { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double Weight { get; set; }
+
+        [Required]
+        public string Color { get; set; }
+
+        [Required]
+        public string Species { get; set; }
+
+        [Required]
+        public string Breed { get; set; }
+
+        [MaxLength(10)]
+        public IList<ExtraPetOptionsDTO> PetOptions { get; set; } = [];
+    }
+}
---
+++ src/Contact-zoo-at-home.Shared/Dto/ExtraPetOptionsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_zoo_at_home.Shared.Dto
+{
+    public class ExtraPetOptionsDTO
+    {
+        public int Id { get; set; }
+        public string OptionName { get; set; }
+        public string OptionValue { get; set; }
+    }
+}
