2cb91c07c0ba5d4c7bb6cdbf1d370b838e14e23f
Roman Havriluk <pomahtik_inc@outlook.com>
2024-04-14 10:57:36 +0300

    create pet view

--- src/Contact-zoo-at-home.WebUI/Controllers/UserSettingsController.cs
+++ src/Contact-zoo-at-home.WebUI/Controllers/UserSettingsController.cs
@@ -49,5 +49,35 @@ namespace Contact_zoo_at_home.WebUI.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        [Route("pets/create")]
+        public IActionResult CreateNewPet()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("pets/create")]
+        public async Task<IActionResult> CreateNewPet(CreateRedactPetDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Invalid model");
+                return View(model);
+            }
+
+            try
+            {
+                await HttpContext.MakeApiPostRequestAsync("settings/pets", model);
+            }
+            catch
+            {
+                ModelState.AddModelError("", "something went wrong. Aka Bad request.");
+            }
+
+            return View(model);
+
+        }
+
     }
 }
