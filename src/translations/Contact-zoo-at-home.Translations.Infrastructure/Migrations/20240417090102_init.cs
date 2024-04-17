f715ff0cb7eaaf05d1392490a2fabf67a84d630c
Roman Havriluk <pomahtik_inc@outlook.com>
2024-04-17 13:00:54 +0300

    migrations commit

---
+++ src/translations/Contact-zoo-at-home.Translations.Infrastructure/Migrations/20240417090102_init.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Contact_zoo_at_home.Translations.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Translations");
+
+            migrationBuilder.CreateTable(
+                name: "Species",
+                schema: "Translations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Language = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Species", x => new { x.Id, x.Language });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Species",
+                schema: "Translations");
+        }
+    }
+}
